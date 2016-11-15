06daaa1fe5727361807d921bf9012c7616e1dad9
Dmitry Kutsenko <d2emonium@gmail.com>
2016-11-15 15:46:54 +0400

    New units

--- users.dpr
+++ users.dpr
@@ -20,9 +20,9 @@ begin
   if(User.ShowLoginForm)then
   begin
     Application.CreateForm(TfmMain, fmMain);
-  Application.CreateForm(TdmGame, dmGame);
-  Application.CreateForm(TfmSelect, fmSelect);
-  //Application.CreateForm(TdmUser, dmUser);
+    Application.CreateForm(TfmSelect, fmSelect);
+    Application.CreateForm(TdmGame, dmGame);
+    //Application.CreateForm(TdmUser, dmUser);
     //Application.CreateForm(TfmRegister, fmRegister);
     //Application.CreateForm(TfmLogin, fmLogin);
     Application.Run;
